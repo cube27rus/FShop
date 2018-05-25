f9c5b17dbc07de1fa08ab497c17f6333bbe29438
Igor Bondarenko <npro27@yandex.ru>
2018-05-25 23:12:22 +1000

    Add Basket Model

--- FShop/FShop.DataAccess.SQL/DataContext.cs
+++ FShop/FShop.DataAccess.SQL/DataContext.cs
@@ -17,7 +17,7 @@ namespace FShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
-
-
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
